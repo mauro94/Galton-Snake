88daeb8538b78f956edac0fbc9af29692d43a4a4
psanchezp <psanchezp94@gmail.com>
2017-04-16 17:20:00 -0500

    WIP Virtual machine skeleton

--- VirtualMachine.py
+++ VirtualMachine.py
@@ -6,6 +6,7 @@ def virtualMachine (functions, quadruples, global_variables, local_variables, te
   #Memory stuff
 
   for q in quadruples:
+    #Arithmetic
     # Addition
     if q['operator'] == '+':
       leftOperand = q['operand1']
@@ -44,3 +45,85 @@ def virtualMachine (functions, quadruples, global_variables, local_variables, te
       result = q['result']
 
       # Store value in memory
+
+    # Comparison
+    # <, >, <=, >=, ==, !=, &&, ||
+    elif q['operator'] == '<':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    elif q['operator'] == '>':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    elif q['operator'] == '<=':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    elif q['operator'] == '>=':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    elif q['operator'] == '==':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    elif q['operator'] == '!=':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    elif q['operator'] == '||':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    elif q['operator'] == '&&':
+      leftOperand = q['operand1']
+      rightOperand = q['operand2']
+      result = q['result']
+
+    # Other operators
+    # Param
+    elif q['operator'] == 'param':
+      leftOperand = q['operand1']
+      result = q['result']
+
+    # Return
+    elif q['operator'] == 'Return':
+      leftOperand = q['operand1']
+      result = q['result']
+
+    # GoSub
+    elif q['operator'] == 'GoSub':
+      leftOperand = q['operand1']
+
+    # GotoF
+    elif q['operator'] == 'GoToF':
+      leftOperand = q['operand1']
+      result = q['result']
+
+    # Goto
+    elif q['operator'] == 'GoTo':
+      result = q['result']
+
+    # ERA
+    elif q['operator'] == 'era':
+      leftOperand = q['operand1']
+
+    # Print
+    # elif q['operator'] == 'print'
+
+    # Read
+    # elif q['operator'] == 'read'
+
+    # ENDPROC
+    elif q['operator'] == 'EndProc':
+
+    # END
+    # elif q['operator'] == 'End'
